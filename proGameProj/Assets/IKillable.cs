40266b714047bf0df0932c5c9b83e9f4fb0ceb9c
Dakota Polenz <polenzdakota@yahoo.com>
2015-10-22 18:32:58 -0700

    Added IKillable and IPickup interfaces

---
+++ proGameProj/Assets/IKillable.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Interface for killable units
+/// </summary>
+public interface IKillable {
+
+	/// <summary>
+	/// Movement change for a unit. Called each frame.
+	/// </summary>
+	void Movement();
+
+	/// <summary>
+	/// Executed when unit is killed
+	/// </summary>
+	void OnKill();
+
+	/// <summary>
+	/// Executed when unit is hit.
+	/// </summary>
+	void OnHit();
+
+}
---
+++ proGameProj/Assets/IPickup.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Interface for any powerup or pickup.
+/// </summary>
+public interface IPickup {
+
+	/// <summary>
+	/// Movement change for a unit. Called each frame
+	/// </summary>
+	void Movement();
+
+	/// <summary>
+	/// Condition given to the player state when pickup is pickuped.
+	/// </summary>
+	void UpdateCondition();
+}
