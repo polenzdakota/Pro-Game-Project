700f339a173cbce73182b340af4c0a8744880786
Dakota Polenz <polenzdakota@yahoo.com>
2015-10-23 00:25:32 -0700

    Added PlayerScript and basic input reader
    Basic player unit prefab added for testing purposes. I also adjusted the
    sensitivity and gravity of the inputs via the project settings.

---
+++ proGameProj/Assets/PlayerScript.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour, IKillable {
+	public float speed = 2f; 
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		CheckInputs ();
+	}
+
+	/// <summary>
+	/// Checks the inputs each frame.
+	/// </summary>
+	void CheckInputs() {
+		//For now check inputs will handle movement
+		float movementX = Input.GetAxis ("Horizontal");
+		float movementY = Input.GetAxis ("Vertical");
+		transform.Translate (speed * movementX, speed * movementY, 0);
+
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			Application.Quit ();
+		}
+	}
+
+	/// <summary>
+	/// Handles non-player inputed movement
+	/// </summary>
+	public void Movement() {
+		//TODO
+	}
+
+	/// <summary>
+	/// Executed when unit is killed
+	/// </summary>
+	public void OnKill() {
+		//TODO
+	}
+
+	/// <summary>
+	/// Executed when unit is hit.
+	/// </summary>
+	public void OnHit() {
+		//TODO
+	}
+}
