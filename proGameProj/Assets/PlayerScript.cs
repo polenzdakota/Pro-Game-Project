4375c9b85523566ebfe64d481b493347bc54d68b
Dakota Polenz <polenzdakota@yahoo.com>
2015-10-23 01:02:18 -0700

    Added isWithinBounds method to restrict player movement
    All variables to alter the playfield are public. Check inputs calls
    isWithinBounds to see if next movement call is within the playfield.

--- proGameProj/Assets/PlayerScript.cs
+++ proGameProj/Assets/PlayerScript.cs
@@ -2,11 +2,15 @@
 using System.Collections;
 
 public class PlayerScript : MonoBehaviour, IKillable {
-	public float speed = 2f; 
+	public float speed = 2f;
+	public int topLeftPlayFieldX = -20;
+	public int topLeftPlayFieldY = 10;
+	public int playFieldWidth = 40;
+	public int playFieldHeight = 20;
+
 
 	// Use this for initialization
 	void Start () {
-	
 	}
 	
 	// Update is called once per frame
@@ -21,11 +25,28 @@ public class PlayerScript : MonoBehaviour, IKillable {
 		//For now check inputs will handle movement
 		float movementX = Input.GetAxis ("Horizontal");
 		float movementY = Input.GetAxis ("Vertical");
-		transform.Translate (speed * movementX, speed * movementY, 0);
+
+		if (isWithinBounds (movementX, movementY, speed)) {
+			transform.Translate (speed * movementX, speed * movementY, 0);
+		}
 
 		if (Input.GetKeyDown (KeyCode.Escape)) {
 			Application.Quit ();
 		}
+	}
+
+	/// <summary>
+	/// Returns true if the next point is within the playfield.
+	/// </summary>
+	/// <returns><c>true</c>, if within bounds <c>false</c> otherwise.</returns>
+	/// <param name="xMovement">X movement direction and degree</param>
+	/// <param name="yMovement">Y movement direction and degree</param>
+	/// <param name="speed">Speed of the player unit</param>
+	bool isWithinBounds(float xMovement, float yMovement, float speed) {
+		float xPos = transform.position.x + (xMovement * speed);
+		float yPos = transform.position.y + (yMovement * speed);
+		return ((xPos > topLeftPlayFieldX && xPos < topLeftPlayFieldX + playFieldWidth) &&
+			    (yPos < topLeftPlayFieldY && yPos > topLeftPlayFieldY - playFieldHeight));
 	}
 
 	/// <summary>
