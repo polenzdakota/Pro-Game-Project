0df929bc6ed562daba8be6b08e344d3df7a0dda4
Dakota Polenz <polenzdakota@yahoo.com>
2015-11-07 14:38:08 -0800

    Changed one line

--- proGameProj/Assets/Scripts/RestartScript.cs
+++ proGameProj/Assets/Scripts/RestartScript.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class RestartScript : MonoBehaviour {
 	
 	public void RestartGame() {
-		Application.LoadLevel (Application.loadedLevel);
+		Application.LoadLevel ("startScene");
 	}
 }
 
