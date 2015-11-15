b2684c56e9ed54f7fae7cc352d26ca2033d20d28
Sapna <sapnanyyr@yahoo.com>
2015-11-15 15:21:57 -0800

    Added Background and corresponding background scrolling for parallax illusion

---
+++ proGameProj/Assets/Scripts/BackgroundScroller.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundScroller : MonoBehaviour {
+
+	public float scrollSpeed;
+	public float tileSizeZ;
+	// Use this for initialization
+
+	private Vector3 startPosition;
+
+	void Start () {
+		startPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float newPosition = Mathf.Repeat (Time.time * scrollSpeed, tileSizeZ);
+		transform.position = startPosition + Vector3.forward * newPosition;
+	
+	}
+}
