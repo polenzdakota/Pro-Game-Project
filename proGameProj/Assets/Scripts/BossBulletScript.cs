e03451352f4db75be0134266c499fcedc24bf5f1
Dakota Polenz <polenzdakota@yahoo.com>
2015-11-16 05:18:19 -0800

    Added boss and completed level
    Going to take a break now will do more later. All that is needed now
    just some cleanup and adding a few minor features.

---
+++ proGameProj/Assets/Scripts/BossBulletScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BossBulletScript : MonoBehaviour {
+	public float bulletSpeed = 2f;
+	private float iniPosY;
+	private float maxY;
+	
+	// Use this for initialization
+	void Start () {
+		iniPosY = transform.position.y;
+		maxY = iniPosY - 25;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (0, -bulletSpeed, 0);
+		if (transform.position.y < maxY) {
+			DestroyObject (gameObject);
+		}
+		
+	}
+	
+	/// <summary>
+	/// Raises the trigger enter event.
+	/// </summary>
+	/// <param name="col">Col.</param>
+	void OnTriggerEnter(Collider col) {
+		if (col.GetComponent<PlayerScript>() != null) {
+			col.GetComponent<PlayerScript>().OnHitP();
+			DestroyObject (gameObject);
+		}
+	}
+}
---
+++ proGameProj/Assets/Scripts/EnemyAI/BossScript.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Class for the movement and behavior that fires a bullet
+/// every few seconds
+/// </summary>
+public class BossScript : MonoBehaviour, IKillable {
+	
+	private Vector3 originalPos;
+	public float speed = 0.1f;
+	public int hp = 10;
+	private float movementX;
+	private float movementY;
+	public float frequency = 1f;
+	private float startTime;
+	private float prevTime = 0.0f;
+	private float timedFire;
+	public GameObject UI;
+	public GameObject control;
+	public GameObject player;
+	public GameObject EnemBullet;
+	
+	// Use this for initialization
+	void Start () {
+		startTime = Time.time;
+		movementX = 0;
+		movementY = 0;
+		originalPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timedFire = Time.time - startTime - prevTime;
+		Rect activeRect = control.GetComponent<CameraAndBoardControl> ().GetCamView ();
+		Vector2 currentPos = new Vector2 (transform.position.x, transform.position.y);
+		float offset = activeRect.y + 30f; 
+		if (currentPos.y < offset) {
+			Movement();
+			if (timedFire > frequency) {
+				Instantiate (EnemBullet, transform.position, transform.rotation);
+				prevTime = Time.time;
+				timedFire = 0;
+			}
+		}
+	}
+	
+	/// <summary>
+	/// Movement change for a unit. Called each frame.
+	/// </summary>
+	public void Movement(){
+		Vector3 playerPos = player.GetComponent<PlayerScript>().GetPosition();
+		movementY = control.GetComponent<CameraAndBoardControl>().GetCamSpeed();
+		movementX = (transform.position.x > playerPos.x) ? -1 : 1;
+		transform.Translate (movementX * speed, movementY, 0);
+	}
+	
+	/// <summary>
+	/// Executed when unit is hit.
+	/// </summary>
+	public void OnKill() {
+		DestroyObject (gameObject);
+		UI.GetComponent<UnityUIController> ().UpdateScore (200);
+	}
+	
+	/// <summary>
+	/// Executed when unit is killed
+	/// </summary>
+	public void OnHit() {
+		hp--;
+		UI.GetComponent<UnityUIController> ().UpdateScore (25);
+		if (hp <= 0)
+			OnKill ();
+	}
+}
